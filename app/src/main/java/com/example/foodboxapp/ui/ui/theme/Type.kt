3a1f1b7dfe237c04c98e26048d3acff68eada3aa
Manino203 <maninohamran@gmail.com>
2024-06-11 12:16:29 +0200

    feat: theme

--- app/src/main/java/com/example/foodboxapp/ui/ui/theme/Type.kt
+++ app/src/main/java/com/example/foodboxapp/ui/ui/theme/Type.kt
@@ -1,10 +1,4 @@
-package com.example.ui.theme
-
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+package com.example.foodboxapp.ui.ui.theme
 
 
 
