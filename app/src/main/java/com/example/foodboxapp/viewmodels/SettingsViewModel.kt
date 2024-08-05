36ebd71c7c0076f67f6092532be1f487cbd55e46
Manino203 <maninohamran@gmail.com>
2024-08-05 22:34:45 +0200

    feat: change theme and locale in settings

--- app/src/main/java/com/example/foodboxapp/viewmodels/SettingsViewModel.kt
+++ app/src/main/java/com/example/foodboxapp/viewmodels/SettingsViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.foodboxapp.viewmodels
 
 import android.content.Context
-import android.content.res.Configuration
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -11,7 +10,6 @@ import com.example.foodboxapp.backend.repositories.SettingsRepositoryImpl.Compan
 import com.example.foodboxapp.backend.repositories.SettingsState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.Locale
 
 class SettingsViewModel(
     private val settingsRepo: SettingsRepository
@@ -29,16 +27,10 @@ class SettingsViewModel(
 
     fun setLanguage(index: Int, context: Context){
         settingsRepo.update(uiState.value.copy(language = LANGUAGE_CHOICES[index]))
-        context.resources.updateConfiguration(Configuration().apply {
-                setLocale(Locale("sk"))
-            },
-            context.resources.displayMetrics
-        )
     }
 
     fun setTheme(index: Int){
         settingsRepo.update(uiState.value.copy(theme = THEME_CHOICES[index]))
-
     }
 
 }
